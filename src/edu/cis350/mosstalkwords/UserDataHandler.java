fd1cb6918415ac0b0fc81a55c70cf79c368ab05a
karthikalle <karthikalle@vagvlan531.089.wlan.dria.upenn.edu>
2013-11-11 20:12:52 -0500

    The function getFavoriteStimulus() has been changed to return a list of Image objects.

--- src/edu/cis350/mosstalkwords/UserDataHandler.java
+++ src/edu/cis350/mosstalkwords/UserDataHandler.java
@@ -9,29 +9,40 @@ import android.content.Context;
 
 public class UserDataHandler {
         
-        public List<UserStimuli> getFavoriteStimulus(Context context)
+        public List<Image> getFavoriteStimulus(Context context)
         {
                 List<UserStimuli> usList = new ArrayList<UserStimuli>();
                 DatabaseHandler dbHandler = new DatabaseHandler(context);
-                
+                List<Image> imgList = new ArrayList<Image>();
+
                 usList = dbHandler.getFavoriteStimuli();
                 
                 
                 Collections.shuffle(usList);
                 
-                if(usList.size() < 20)
+                /*if(usList.size() < 20)
                 {
                         return usList;
                 }
-                else
-                {
-                        List<UserStimuli> usListFinal = new ArrayList<UserStimuli>();
-                        for(int i=0;i < 20;i++)
-                        {
-                                usListFinal.add(usList.get(i));
-                        }
-                        return usListFinal;
-                }
+                //else
+                //{
+                        
+                if(usList.size()>20) {        
+                //	List<UserStimuli> usListFinal = new ArrayList<UserStimuli>();*/
+                
+                	for(int i=0;i < 20;i++)
+	                	    {
+	                            imgList.get(i).setCategory(usList.get(i).getCategory());
+	                            imgList.get(i).setWord(usList.get(i).getImageName());
+	                            imgList.get(i).setUrl(usList.get(i).getUrl());
+	                            imgList.get(i).setFrequency(0);
+	                            imgList.get(i).setLength(0);
+	                            imgList.get(i).setImageability(0);
+	                            if(i+1 == 20 || i+1 == usList.size())
+	                            	break;
+	                        }
+	           	
+                return imgList;
                 
         }
 }
