3d95acb560bf4a378493fffeac8afb5549908eae
cpshah1507 <chitrang@modvvlan502.0138.wlan.wharton.upenn.edu>
2013-11-17 21:07:27 -0500

    1. Added backendtest 2. Modifed userdatahandler to remove unnecessary commented code 3. Added a constructor in UserStimuli to use in testing 4. Added Deletetable method in databaseHandler for testing

--- src/edu/cis350/mosstalkwords/UserDataHandler.java
+++ src/edu/cis350/mosstalkwords/UserDataHandler.java
@@ -23,37 +23,19 @@ public class UserDataHandler {
 
 		usList = dbHandler.getFavoriteStimuli();
 
-
 		Collections.shuffle(usList);
 		System.out.println(usList.size());
-		System.out.println(usList.get(0).getImageName());
-		/*if(usList.size() < 20)
-	                {
-	                        return usList;
-	                }
-	                //else
-	                //{
-
-	                if(usList.size()>20) {        
-	                //	List<UserStimuli> usListFinal = new ArrayList<UserStimuli>();*/
-
-
-		for(int i=0;i < 20;i++)
+		if(usList.size() > 0)
 		{
-			Image img = new Image(usList.get(i).getImageName(), usList.get(i).getCategory(), 
-					"0", "0", "0", usList.get(i).getUrl());
-			imgList.add(img);
-			//imgList.add(setCategory(usList.get(i).getCategory());
-			//imgList.get(i).setWord(usList.get(i).getImageName());
-		//	imgList.get(i).setUrl(usList.get(i).getUrl());
-		//	imgList.get(i).setFrequency(0);
-		//	imgList.get(i).setLength(0);
-		//	imgList.get(i).setImageability(0);
-			
-			if(i+1 == 20 || i+1 == usList.size())
-				break;
+			for(int i=0;i < 20;i++)
+			{
+				Image img = new Image(usList.get(i).getImageName(), usList.get(i).getCategory(), 
+						"0", "0", "0", usList.get(i).getUrl());
+				imgList.add(img);
+				if(i+1 == 20 || i+1 == usList.size())
+					break;
+			}
 		}
-
 		return imgList;
 
 	}
